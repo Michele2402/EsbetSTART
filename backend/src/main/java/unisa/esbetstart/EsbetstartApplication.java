3bda3e06ec2afddc140be4645f696448e467a769
Michele Polise <mpolise@tpositive.it>
2024-10-06 17:13:04 +0200

    backend setup

---
+++ backend/src/main/java/unisa/esbetstart/EsbetstartApplication.java
@@ -0,0 +1,13 @@
+package unisa.esbetstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbetstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbetstartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/unisa/esbetstart/EsbetstartApplicationTests.java
@@ -0,0 +1,13 @@
+package unisa.esbetstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbetstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
