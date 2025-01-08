557ed5bac9dacc1324d6be488b345c50b0165f9b
Ritopla <f.petrillo16@studenti.unisa.it>
2025-01-08 12:14:34 +0100

    First implementation of ActivatedOffer

--- backend/src/main/java/unisa/esbetstart/transactionmanagment/domain/model/ActivatedOffer.java
+++ backend/src/main/java/unisa/esbetstart/transactionmanagment/domain/model/ActivatedOffer.java
@@ -1,5 +1,25 @@
 package unisa.esbetstart.transactionmanagment.domain.model;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import unisa.esbetstart.usermanagment.domain.model.Gambler;
 
+@NoArgsConstructor
+@Data
+@Slf4j
 public class ActivatedOffer {
+
+    private Gambler gambler;
+    private Offer offer;
+    private double progress;
+
+    @Builder
+    public ActivatedOffer(Gambler gambler, Offer offer, double progress) {
+        this.gambler = gambler;
+        this.offer = offer;
+        this.progress = progress;
+    }
+
 }
