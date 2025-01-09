0b844891127279cffd3035e58a42866bdf1c2d74
Ritopla <f.petrillo16@studenti.unisa.it>
2025-01-09 21:25:41 +0100

    Implemented last methods of Gambler

--- backend/src/main/java/unisa/esbetstart/transactionmanagment/domain/model/ActivatedOffer.java
+++ backend/src/main/java/unisa/esbetstart/transactionmanagment/domain/model/ActivatedOffer.java
@@ -6,17 +6,21 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import unisa.esbetstart.usermanagment.domain.model.Gambler;
 
+import java.util.UUID;
+
 @NoArgsConstructor
 @Data
 @Slf4j
 public class ActivatedOffer {
 
+    private UUID id;
     private Gambler gambler;
     private Offer offer;
     private double progress;
 
     @Builder
-    public ActivatedOffer(Gambler gambler, Offer offer, double progress) {
+    public ActivatedOffer(UUID id, Gambler gambler, Offer offer, double progress) {
+        this.id = id;
         this.gambler = gambler;
         this.offer = offer;
         this.progress = progress;
