78f02fd224c33a284db583e2d38c671bf01a464c
Michele Polise <mpolise@tpositive.it>
2025-01-15 00:22:28 +0100

    event manager page, backend API call

---
+++ frontend/src/app/core/services/game-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {environmentPaths} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {GameWithRulesResponse} from "../../model/response/game-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameServiceService {
+
+  private basePath = environmentPaths.base_path;
+
+  constructor(private http: HttpClient) { }
+
+  getAllGames(): Observable<GameWithRulesResponse[]> {
+    return this.http.get<GameWithRulesResponse[]>(this.basePath + environmentPaths.get_all_games);
+  }
+}
