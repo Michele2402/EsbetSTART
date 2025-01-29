82f13fe842630b6a2a91508589b392c30c1031e2
Michele Polise <mpolise@tpositive.it>
2025-01-29 19:45:59 +0100

    profile left bar

---
+++ frontend/src/app/shared/components/profile-leftbar/profile-leftbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatCard} from "@angular/material/card";
+import {MatDivider} from "@angular/material/divider";
+import {MatFabAnchor} from "@angular/material/button";
+import {MatList, MatListItem} from "@angular/material/list";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-profile-leftbar',
+  templateUrl: './profile-leftbar.component.html',
+  styleUrl: './profile-leftbar.component.css'
+})
+export class ProfileLeftbarComponent {
+
+}
